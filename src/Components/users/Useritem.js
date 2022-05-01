70a8cab69386a9bc1d9be0815facb1c2455a4f8b
DJ.Knight <djknight90@outlook.com>
2022-05-01 19:11:15 +1000

    slight refector

--- src/Components/users/Useritem.js
+++ src/Components/users/Useritem.js
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      login: "mojombo",
-      id: 1,
-      node_id: "MDQ6VXNlcjE=",
-      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-      gravatar_id: "",
-      url: "https://api.github.com/users/mojombo",
-      html_url: "https://github.com/mojombo",
-    };
-  }
+  state = {
+    login: "mojombo",
+    id: 1,
+    node_id: "MDQ6VXNlcjE=",
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/mojombo",
+    html_url: "https://github.com/mojombo",
+  };
 
   render() {
+    const { login, avatar_url, html_url } = this.state;
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=""
           className="round-img"
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
             more
           </a>
         </div>
