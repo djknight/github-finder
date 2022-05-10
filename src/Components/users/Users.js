7c54e0729b1a831f26fec16da6ad0ecbc03db363
DJ.Knight <djknight90@outlook.com>
2022-05-10 20:45:48 +1000

    api fix?

--- src/Components/users/Users.js
+++ src/Components/users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./Useritem";
 import Spinner from "../layout/spinner";
 import PropTypes from "prop-types";
