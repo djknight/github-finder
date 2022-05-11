6714f89e93b0ea3d885108344aa25a4abbbb3671
DJ.Knight <djknight90@outlook.com>
2022-05-11 20:47:18 +1000

    submitting & event para

--- src/Components/users/Search.js
+++ src/Components/users/Search.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 
 export class search extends Component {
+  state = {
+    Text: "",
+  };
+  //submit the form
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+  //event para
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
-        <form className="form"></form>
-        <input type="text" Name="search" placeholder="Search Users..."></input>
-        <input
-          type="submit"
-          value="submit"
-          className="btn btn-dark btn-block"
-        ></input>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+
+          <input
+            type="submit"
+            value="search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
       </div>
     );
   }
